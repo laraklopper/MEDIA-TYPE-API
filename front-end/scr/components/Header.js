175907ff8e40b6c84d9289901bba07e6d274de9c
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-10-04 12:37:05 +0200

    Update Header.js

--- front-end/scr/components/Header.js
+++ front-end/scr/components/Header.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import SearchBar from './Search'
 
+export default function Header() {
+  return (
+    <div>
+        <header id="header">
+            <h1 className="h1">Itunes Search API</h1> 
+            <SearchBar/>
+        </header>
+    </div>
+  )
+}
