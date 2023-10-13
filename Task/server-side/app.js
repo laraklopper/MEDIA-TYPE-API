6a432dd0c3b5520f96a4d2d9728af765f85dff56
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-10-13 16:27:46 +0200

    Update app.js

--- Task/server-side/app.js
+++ Task/server-side/app.js
@@ -14,42 +14,32 @@ let items = [];
 
 //====================GET REQUEST==========================
 app.get('/submit-form', (req, res) => {
-    res.json([term, 'Default term']);//Respond with a default term
+    const term = req.query.term
+    const type = req.query.type
+
+    / Perform some logic based on term and type
+    // ...
+
+    // Send a response back to the client
+    res.json({ term, type, message: 'Received your request!' });
 });
 //==================POST REQUEST=========================
 // Endpoint to handle adding a new item
-app.post('/api/add-item', (req, res),=> {
-    const newItem = req.body;  // Get the new item from the request body
-    items.push(newItem);//Add new item to data storage
-    res.json(newItem);//Respond with added item
-} )
+app.post('/api/add-item', (req, res) => {
+    try {
+        const newItem = req.body;
+        if (!newItem) {
+            throw new Error('Invalid data received');
+        }
+
+        items.push(newItem);
+        res.json(newItem);
+    } catch (error) {
+        console.error('Error adding item:', error.message);
+        res.status(500).json({ error: 'Failed to add new item' });
+    }
+});
 //==================START THE SERVER=============================
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);//Display a message in the console indicating that the server is running.
-});
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-
-// Simulated data storage
-let items = [];
-
-app.get('/submit-form', (req, res) => {
-  res.json([{ term: 'Default Term' }]);
-});
-
-app.post('/api/add-item', (req, res) => {
-  const newItem = req.body;
-  items.push(newItem);
-  res.json(newItem);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
