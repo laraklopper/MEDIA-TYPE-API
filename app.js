d26fd68db04206d3a72185cc2abc62e4914febf5
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-10-13 08:31:13 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-//const fs = require('fs');
-//const bodyParser = require('body-parser');
-const app = express()
-const port = process.env.PORT || 8000;
-
-app.listen(port, () =>{
-    console.log(`app is listening in" ${port}`);
-})
