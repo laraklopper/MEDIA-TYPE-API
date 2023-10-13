731fb88323c42582841161d63689881b9a0d2dab
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-10-13 15:40:47 +0200

    Update app.js

--- Task/server-side/app.js
+++ Task/server-side/app.js
@@ -9,14 +9,47 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-//====================GET REQUES==========================
-app.get('/api/submit-form', (req, res) => {
-    const formData = req.body;
-    console.log('Form Data Received:', formData);
-    res.json({ formData });
-});
+//-------------Data storage----------------
+let items = [];
 
+//====================GET REQUEST==========================
+app.get('/submit-form', (req, res) => {
+    res.json([term, 'Default term']);//Respond with a default term
+});
+//==================POST REQUEST=========================
+// Endpoint to handle adding a new item
+app.post('/api/add-item', (req, res),=> {
+    const newItem = req.body;  // Get the new item from the request body
+    items.push(newItem);//Add new item to data storage
+    res.json(newItem);//Respond with added item
+} )
 //==================START THE SERVER=============================
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);//Display a message in the console indicating that the server is running.
+});
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// Simulated data storage
+let items = [];
+
+app.get('/submit-form', (req, res) => {
+  res.json([{ term: 'Default Term' }]);
+});
+
+app.post('/api/add-item', (req, res) => {
+  const newItem = req.body;
+  items.push(newItem);
+  res.json(newItem);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
