1b1c943aa3584dc2c7c0e44fae52f31d3bacb627
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-10-16 13:26:50 +0200

    Update App.js

--- Task/server-side/client-side/src/App.js
+++ Task/server-side/client-side/src/App.js
@@ -1,103 +1,124 @@
-import React, {  useEffect, useState, } from 'react';// Import the React module to use React functionalities
-import './App.css';//Import css stylesheet
-import Button from 'react-bootstrap/Button'; // Import Bootstrap Button component
+import React, { useState, useEffect } from 'react';// Import the React module to use React functionalities
+import './App.css';//Import CSS stylesheet
+import Form from './components/Form';//Import Form function component
 
 //App function component
-export default function App() {//Export default function component
-  //========== State variables=====================
-   const [error, setError] = useState(null);// State for error handling
+export default function App() {//Export default App function component
+  //===========STATE VARIABLES============================
+  const [error, setError] = useState(null);// State used to store error information
+  const [isLoaded, setIsLoaded] = useState(false);//State used to track whether data is loaded
   const [term, setTerm] = useState('');// State for the term input
-  const [type, setType] = useState(mediaTypes[0].value);// State for the type input
+  const [type, setType] = useState('');// State for the type input
   const [data, setData] = useState([]);// State to store fetched data
-  const [isLoaded, setIsLoaded] = useState(false);// State to track loading status
 
-    //=============== FETCH API DATA ============
-  // Fetch initial data from the server when the component mounts
+  //===================FETCH JSON DATA=====================
+  // Fetch API data on component mount
   useEffect(() => {
-    async function fetchData() {//Define the asynchronous function to fetch data
+    async function fetchData() {//Define asynchronous function to fetch data
       try {
-        const response = await fetch('http://localhost:3001/api/submit-form');// Fetch request to the specified endpoint
-        // Conditional rendering to check the 'ok' property of the 'response' object is false.
+        const response = await fetch('http://localhost:3001/submit-form');//Make a GET request Fetch data to the specified endpoint
+
+        //Conditional rendering to check the 'ok' property of the response object
         if (!response.ok) {
-          throw new Error('Failed to fetch data');
+          throw new Error();//Throw an error message if the request is unsuccessful
         }
         const jsonData = await response.json();
-        setTerm(jsonData[0].term);
-        setIsLoaded(true);
-      } 
-        
-        catch (error) {// Result if an error occurs during the fetch
-        console.error('Error fetching data:', error);
-        setError('Failed to load the data');
-        setIsLoaded(true);
+        setTerm(jsonData[0].term);// Set the value of the term state to the first object ([0]) of the jsonData array.
+        setIsLoaded(true);//Update the loading status to true
+      } catch (error) {
+        // Handle errors that may occur during the fetch operation
+        console.error('Error fetching data:', error);//Display error message in the console
+        setError('Failed to load the data');//Set an error message in the error state
+        setIsLoaded(true);//Update the loading status to true
       }
     }
-    fetchData();//Call the fetchdata function
+
+    fetchData();
   }, []);// Empty dependency array means this effect runs only once after the initial render
 
-  //=============REQUEST FUNCTIONS====================
-//----------------DEFAULT GET REQUEST-------------------------------
-  // Handle the search form submission
-  const handleSearch = async (event) => {
+  //============================FUNCTIONS TO HANDLE REQUESTS=============================== 
+  //=========================FUNCTION TO SEARCH FOR API DATA==============================
+
+  const handleSearch = async (event) => {//Define asynchronous function 
     event.preventDefault();
     try {
-      const response = await fetch(`https://itunes.apple.com/search?term=${term}+&entity=${type}`);//Api URL
+      // Fetch data from the iTunes API based on 'term' and 'type'
+      const response = await fetch(`https://itunes.apple.com/search?term=${term}&entity=${type}`);
 
-      // Conditional rendering to check the 'ok' property of the 'response' is false.
+      // Conditional rendering to check the 'ok' property of the 'response' object 
       if (!response.ok) {
-        throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+        throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);//Throw an error message if the response is unsuccessful
       }
 
       const searchData = await response.json();
       setData(searchData.results);
-      setError(null); // Clear the error on successful fetch
-      console.log(searchData);
-    } 
-      //Handle errors 
-      catch (error) {// Result if an error occurs during the fetch
+      console.log(searchData);;//Display search results in the console
+    } catch (error) {
       console.error('Error fetching data:', error.message);
-      setError('Failed to load data');
+      setError('Failed to fetch search results');
     }
   };
-
-  //-----------------POST REQUEST-----------------------
-  // Function to add new item to the list
-  const handleAddItem = async () => {//Define asynchronous function
+  //------------------POST REQUEST--------------------------------
+  //Function to add new item
+  const handleAddItem = async () => {//Define the asnychronous function to add a new item
     try {
-      const newItem = {//new Item to be added
+      const newItem = {
         artistName: '',
         trackId: '',
         kind: '',
         trackName: '',
       };
-      const response = await fetch('http://localhost:3001/api/add-item', {
-        method: 'POST',//Request method
+
+      const response = await fetch('http://localhost:3001/api/add-item', {//Request API URL
+        method: 'POST',//Request type
         headers: {
-          'Content-Type': 'application/json',//Type of content being passed
+          'Content-Type': 'application/json',//Type of data being passed
         },
-        body: JSON.stringify({//Convert the JavaScript object{ term, type, newItem} into a JSON string.
-          // The string is the data sent to the request body
-          // term, type, and newItem are variables that contain data to be sent to the server.
-          term: term, //Url query parameter 
-          type: type, //URL query parameter
-          newItem: newItem, //newItem is an object representing the new item to be added. 
+        body: JSON.stringify({
+          term: term,
+          type: type,
         }),
       });
 
       // Conditional rendering to check the 'ok' property of the 'response' object is false.
       if (!response.ok) {
-        throw new Error('Failed to add new item');
+        throw new Error('Failed to add new item');//Throw an error message if the POST requwst is unsuccussful
       }
-        const addedItem = await response.json();
-      setData((prevData) => [...prevData, addedItem]); //Add a new item to the existing data
-      //prevData is a parameter representing the previous state. The name prevData suggests that it's an array of data.
-      //The spread operator (...) is used to include all the elements of prevData in the new array.
+
+      setData((prevData) => [...prevData, newItem]);
     } catch (error) {
+      // Handle errors that may occur during the fetch operation
       console.error('Error adding item:', error.message);
+      setError('Failed to add new item');
     }
   };
-   //===============MEDIA TYPES===================
-  // Media types for the dropdown
+
+  //--------------------------DELETE REQUEST-----------------------------
+  //Function to delete an item
+  const deleteItem = async (trackId) => {//Define asynchronous function to delete an item 
+    try {
+      const response = await fetch(`http://localhost:3001/api/${trackId}`, {
+        method: 'DELETE',//Request method
+        headers: {
+          'Content-Type': 'application/json',//Type of content being passed
+        },
+      });
+
+      // Check if the DELETE request is successful; otherwise, throw an error
+      if (!response.ok) {
+        throw new Error('Failed to delete item');//Throw an error with the error message if the Delete request is unsuccessful
+      }
+      // Update the 'data' state by filtering out the deleted item
+      setData((prevData) => prevData.filter((track) => track.trackId !== trackId));
+      console.log(response);//Display response in the console
+    }
+    catch (error) {
+      //Handle errors that may occur during the fetch operation
+      setError('Error deleting the item: ' + error.message);// Set an error message in the 'error' state
+    }
+  };
+
+  //=========== MEDIA TYPES ARRAY===================
   const mediaTypes = [
     { name: 'MUSIC', value: 'music' },
     { name: 'MUSIC VIDEO', value: 'musicVideo' },
@@ -110,59 +131,52 @@ export default function App() {//Export default function component
     { name: 'SHORT FILM', value: 'shortFilm' },
   ];
 
-  //===================JSX RENDERING===============
-  
+  // =================JSX RENDERING======================
+
   return (
     <>
-      {/* Header */}
       <div id="appBody">
-        <header id='header'>
-          <h1 className='h1'>ITUNES API</h1>
+        {/* Header */}
+        <header id="header">
+          <h1 className="h1">ITUNES API</h1>
         </header>
-        {/* Section1 */}
-        <section id='section1'>
-          {/* Form for user input */}
-          <form id="form" onSubmit={handleSearch}>
-            {/* Input field for 'term' */}
-            <label className='label'>NAME</label>
-            <input
-              type="text"
-              placeholder="name"
-              value={term}
-              onChange={(e) => setTerm(e.target.value)}
-            />
-
-            {/* Dropdown for 'type' */}
-            <label className='label'>MEDIA TYPE</label>
-            <select value={type} onChange={(e) => setType(e.target.value)}>
-              {mediaTypes.map((mediaType) => (
-                <option key={mediaType.value} value={mediaType.value}>
-                  {mediaType.name}
-                </option>
-              ))}
-            </select>
-
-            {/* Submit button */}
-            <Button type="submit" variant="primary" id='searchBtn'>
-              SEARCH
-            </Button>
-          </form>
+        {/* Section 1 */}
+        <section id="section1">
+          {/* Use the FormComponent and pass required props */}
+          <Form
+            term={term}
+            setTerm={setTerm}
+            type={type}
+            setType={setType}
+            handleSearch={handleSearch}
+            handleAddItem={handleAddItem}
+            mediaTypes={mediaTypes}
+          />
         </section>
         {/* Section 2 */}
-        <section id='section2'>
-          {/* Display search results or loading/error message */}
+        <section id="section2">
           {error ? (
             <div className="errorMessage">{error}</div>
           ) : !isLoaded ? (
             <p>Loading...</p>
-          ) : data.length > 0 ? (
-            <ul>
+          ) : (
+            <ul id="itemsList">
               {data.map((item) => (
-                <li key={item.trackId}>{item.trackName}</li>
+                <li key={item.trackId}>
+                  <h2 className='h2'>FAVOURITES</h2>
+                  <h3>{item.artistName}</h3>
+                  <h3>TITLE: {item.trackName}</h3>
+                  <h3>TYPE: {item.kind}</h3>
+                  {/* Delete button for each item */}
+                  <button
+                    id="deleteBtn"
+                    onClick={() => deleteItem(item.trackId)}
+                  >
+                    DELETE
+                  </button>
+                </li>
               ))}
             </ul>
-          ) : (
-            <p>No results found.</p>
           )}
         </section>
       </div>
