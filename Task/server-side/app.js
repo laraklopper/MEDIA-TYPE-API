298b5217639285653f52dac891a2f065b0f14f1a
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-10-16 13:36:25 +0200

    Update app.js

--- Task/server-side/app.js
+++ Task/server-side/app.js
@@ -1,45 +1,75 @@
-const express = require('express');// Import the Express web framework
-const bodyParser = require('body-parser');//Import the bodyParser module
-const app = express();// Create an instance of the Express application
-const cors = require('cors');//Import the cors module
-const port = process.env.PORT || 3001;// Define the port the server will listen on
+const express = require('express');  // Import Express.js web framework to build the web server
+const bodyParser = require('body-parser');  // Import bodyParser middleware to parse request bodies
+const cors = require('cors');  // Import cors middleware to handle Cross-Origin Resource Sharing
+const app = express();  // Create an Express application
+const port = process.env.PORT || 3001;  // Define the port for the server to listen on
 
-//=========================MIDDLEWARE SETUP======================================
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+//=================SETUP MIDDLEWARE======================
+app.use(bodyParser.urlencoded({ extended: false }));  // Parse incoming requests with payloads attached to it
+app.use(bodyParser.json());  // Returns middleware that parses and requests where the Content-Type matches the type option.
+app.use(cors());  // Enables CORS for handling cross-origin requests.
 
-//-------------Data storage----------------
+//=====================DATA STORAGE============================
+// Array to store items 
 let items = [];
 
-//====================GET REQUEST==========================
+//========================REQUESTS================================
+//------------------GET REQUEST-------------------------
+// Handle GET request to '/submit-form'
 app.get('/submit-form', (req, res) => {
-    const term = req.query.term
-    const type = req.query.type
+    // Extract query parameters from the request
+    const term = req.query.term; // Get the term from the query parameter
+    const type = req.query.type; // Get the type from the query parameter
 
-    / Perform some logic based on term and type
-    // ...
-
-    // Send a response back to the client
+    // Send a JSON response with the extracted parameters
     res.json({ term, type, message: 'Received your request!' });
 });
-//==================POST REQUEST=========================
-// Endpoint to handle adding a new item
+
+//---------------------------POST REQUEST------------------------------
+// Handle POST request to '/api/add-item'
 app.post('/api/add-item', (req, res) => {
     try {
-        const newItem = req.body;
+        const newItem = req.body; // Extract the new item from the request body
+
+        // Check if valid data is received
         if (!newItem) {
             throw new Error('Invalid data received');
         }
 
-        items.push(newItem);
+        items.push(newItem); // Add the new item to the 'items' array
+
+        // Send a JSON response with the added item
         res.json(newItem);
+
     } catch (error) {
+        // Handle errors and send a 500 Internal Server Error response
         console.error('Error adding item:', error.message);
         res.status(500).json({ error: 'Failed to add new item' });
     }
 });
-//==================START THE SERVER=============================
+
+//=========================FUNCTIONS====================================
+// Function to get all items
+function getAllItems() {
+    return items;
+}
+
+// Function to delete an item by trackId
+function deleteItem(trackId) {
+    const index = items.findIndex((item) => item.trackId === trackId); // Callback function
+
+    if (index !== -1) { // Remove the item from the 'items' array
+        items.splice(index, 1);
+    }
+}
+
+// Function to get an item by trackId
+function getItemById(trackId) {
+    return items.find((item) => item.trackId === trackId); // Find the item by trackID
+}
+
+//====================START THE SERVE==================================
+// Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);//Display a message in the console indicating that the server is running.
+    console.log(`Server is listening on port ${port}`); // Display a message in the console indicating that the server is running.
 });
