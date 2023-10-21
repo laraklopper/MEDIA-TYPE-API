a5fda391522699c453ddf87a28f170fcbc78d057
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-10-21 16:35:33 +0200

    Delete Task/backend/front-end/src directory

--- Task/backend/front-end/src/React.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-
-import App from './App.js';
-
-test('renders correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
